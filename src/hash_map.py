cb36c4cc93cc4b6fc845eba77d04bc41a7cc560d
LeifTheDev <161671624+LeifTheDev@users.noreply.github.com>
2025-02-25 14:35:48 +0800

    feat: Add (untested) implementation of HashMap CRUD operations

--- src/hash_map.py
+++ src/hash_map.py
@@ -1,16 +1,19 @@
 from player_list import PlayerList
+from player_node import PlayerNode
+from player import Player
 
 from typing import Any
 
 
 class HashMap:
+    # TODO: Implement Generics
 
     def __init__(self):
 
         self._size: int = 10
         self._length: int = 0
 
-        self._array: list['PlayerList'] = [PlayerList() for i in range(self._size)]
+        self._array: list[PlayerList] = [PlayerList() for i in range(self._size)]
         
     @property
     def size(self) -> int:
@@ -23,11 +26,12 @@ class HashMap:
         self._size = value
 
     def _hash(self, value: Any) -> int:
-        ...
+        # TODO: Implement *custom* hash algorithm
+        return hash(value) % self._size
 
     def add(self, value: Any):
-        index = self._hash(value)
-        # TODO: Convert the value into a Player
+        index: int = self._hash(value)
+        # TODO: Decide whether to convert the value into a Player
         self._array[index].append(value)
 
     def put(self, key: str, value: Any) -> Any:
@@ -39,14 +43,28 @@ class HashMap:
     def remove(self, key: str) -> Any:
         return self.__delitem__(key)
 
+    # Type annotations for the return values are set as any as I have not yet decided whether I want these to return anything
     def __getitem__(self, key: str) -> Any:
-        ...
+        index = self._hash(key)
+
+        item: PlayerNode
+        for item in self._array[index]:
+            if item.key == key:
+                return item.player
 
     def __setitem__(self, key: str, value: Any) -> Any:
-        ...
+        index = self._hash(key)
+
+        self._array[index].remove(key)
+        self._array[index].prepend(value)
+
+        item: PlayerNode
+        for item in self._array[index]:
+            item.player.name = "Hi"
 
     def __delitem__(self, key: str) -> Any:
-        ...
+        index: int = self._hash(key)
+        self._array[index].remove(key)
 
     def __len__(self) -> int:
         ...
