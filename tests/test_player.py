75608f81e477ceffd2fc0322e2dd0a9ef58a1064
LeifTheDev <161671624+LeifTheDev@users.noreply.github.com>
2025-03-11 23:55:02 +0800

    tests: Adjust unittests to match new Player design decisions - allow Player.name to be writable

--- tests/test_player.py
+++ tests/test_player.py
@@ -18,9 +18,9 @@ class TestPlayer(unittest.TestCase):
     def test_player_name_property_read(self):
         self.assertEqual(self.player.name, "Jane Doe")
 
-    def test_player_name_property_write_raised_attribute_error(self):
-        with self.assertRaises(AttributeError):
-            self.player.name = "John Doe"
+    def test_player_name_property_write_updates_name(self):
+        self.player.name = "John Doe"
+        self.assertEqual(self.player.name, "John Doe")
 
 
 if __name__ == '__main__':
