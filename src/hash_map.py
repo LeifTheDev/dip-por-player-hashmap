2acb2d9e504df6fa210795fee96843a779917da5
LeifTheDev <161671624+LeifTheDev@users.noreply.github.com>
2025-03-17 18:38:14 +0800

    docs: Add docstrings to HashMap

--- src/hash_map.py
+++ src/hash_map.py
@@ -1,5 +1,4 @@
 from src.player_list import PlayerList
-from src.player_node import PlayerNode
 from src.player import Player
 
 from typing import Any
@@ -18,31 +17,67 @@ class HashMap:
         
     @property
     def size(self) -> int:
+        """
+        The number of PlayerList's in the array used to store players
+
+        :return: int
+        """
         return self._size
 
     def _hash(self, value: str|Player) -> int:
+        """
+        Hash a player or string using pearson hash
 
+        :param value: str|Player
+        :return: int
+        """
         if isinstance(value, Player):
             return hash(value) % self._size
 
         return Player.pearson_hash(value) % self._size
 
     def add(self, value: Player):
+        """
+        Add a player to the HashMap
+
+        :param value: Player
+        """
         index: int = self._hash(value)
         # TODO: Decide whether to convert the value into a Player
         self._array[index].append(value)
         self._length += 1
 
-    def put(self, key: str, value: Any) -> Any:
+    def put(self, key: str, value: Any):
+        """
+        Update the value with <key> to <value>
+
+        :param key: str
+        :param value: Any
+        """
         return self.__setitem__(key, value)
 
-    def get(self, key: str) -> Any:
+    def get(self, key: str) -> Player:
+        """
+        Return the player at position <key>
+
+        :param key: str
+
+        :return Player:
+        """
         return self.__getitem__(key)
 
-    def remove(self, key: str) -> Any:
+    def remove(self, key: str):
+        """
+        Remove the player at position <key>
+
+        :param key: str
+        """
         return self.__delitem__(key)
 
     def display(self):
+        """
+        Print the content of the HashMap
+        """
         message = f"{self.__class__.__name__}({', '.join([f'{index}: {player_list}' for index, player_list in enumerate(self._array)])})"
         print(message)
 
@@ -59,9 +94,6 @@ class HashMap:
     def __setitem__(self, key: str, value: Any) -> Any:
         index = self._hash(key)
         self._array[index].update(key, value)
-        # TODO: Consider adding update() method to PlayerList
-        # self._array[index].remove(key)
-        # self._array[index].prepend(value)
 
     def __delitem__(self, key: str) -> Any:
         index: int = self._hash(key)
